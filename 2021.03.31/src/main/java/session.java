fb2735f674de2564852842b47a5f552cefdb1b2f
LuoXianS <2320418206@qq.com>
2021-04-01 20:46:20 +0800

    2021.04.01

--- 2021.03.31/src/main/java/session.java
+++ 2021.03.31/src/main/java/session.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 public class session extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -20,9 +21,21 @@ public class session extends HttpServlet {
         HttpSession session = request.getSession();
         //获取sessionID
         String sessionId = session.getId();
-        writer.println("<h>欢迎访问</h>");
-
-
+        writer.println("<h1>欢迎访问</h1>");
+        writer.println(String.format("<h3>sessionID:%s</h3>",sessionId));
+        writer.println("<hr>");
+        writer.println(String.format("<h3>session的创建时间:%s</h3><p></p>",new Date(session.getCreationTime())));
+        writer.println(String.format("<h3>最后访问时间:%s</h3><p></p>",new Date(session.getLastAccessedTime())));
+        String sessionKey  = "countKey";
+        if(session.isNew()||session.getAttribute(sessionKey)==null){
+            session.setAttribute(sessionKey,1);
+            writer.println("<h3>访问次数: 1</h3>");
+        }else{
+            int count = (int)session.getAttribute(sessionKey);
+            count++;
+            session.setAttribute(sessionKey,count);
+            writer.println("访问次数:"+count);
+        }
 
 
 
